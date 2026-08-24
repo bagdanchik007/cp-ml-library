4d8d5d353eb38caeb3af3f5fab0af06baa2f9283
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-08-24 15:49:57 +0200

    feat(api): expose Normalizer through umbrella header

--- include/ml/ml.hpp
+++ include/ml/ml.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 /**
  * @file ml.hpp
  * @brief Main umbrella header for the Cpp ML Library.
@@ -9,10 +10,15 @@
 
 #include "algorithms/linear_regression.hpp"
 #include "algorithms/kmeans.hpp"
+
 #include "preprocessing/standard_scaler.hpp"
+#include "preprocessing/min_max_scaler.hpp"
+#include "preprocessing/normalizer.hpp"
 
 namespace ml {
+
 constexpr int VERSION_MAJOR = 1;
 constexpr int VERSION_MINOR = 0;
 constexpr int VERSION_PATCH = 0;
-}
+
+} // namespace ml

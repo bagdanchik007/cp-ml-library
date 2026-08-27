55b03f6f3bfba17ec0926cef08f5b98560c08610
bagdanchik007 <bogdanskibitskyi@gmail.com>
2026-08-27 20:04:47 +0200

    chore: prepare version 1.0.0

--- include/ml/ml.hpp
+++ include/ml/ml.hpp
@@ -7,20 +7,29 @@
 
 #include "core/matrix/matrix.hpp"
 #include "core/matrix/matrix_utility.hpp"
+#include "version.hpp"
 
 #include "algorithms/linear_regression.hpp"
 #include "algorithms/kmeans.hpp"
+#include "algorithms/decision_tree.hpp"
+#include "algorithms/random_forest.hpp"
+#include "algorithms/dbscan.hpp"
+
+#include "data/dataset.hpp"
+#include "data/csv_reader.hpp"
+
+#include "metrics/model_evaluation.hpp"
+
+#include "model_selection/kfold.hpp"
+#include "model_selection/cross_validation.hpp"
+#include "model_selection/grid_search.hpp"
+
+#include "pipeline/pipeline.hpp"
 
 #include "preprocessing/standard_scaler.hpp"
 #include "preprocessing/min_max_scaler.hpp"
 #include "preprocessing/normalizer.hpp"
 #include "preprocessing/robust_scaler.hpp"
 #include "preprocessing/one_hot_encoder.hpp"
-
-namespace ml {
-
-constexpr int VERSION_MAJOR = 1;
-constexpr int VERSION_MINOR = 0;
-constexpr int VERSION_PATCH = 0;
-
-} // namespace ml
+#include "preprocessing/simple_imputer.hpp"
+#include "preprocessing/train_test_split.hpp"
---
+++ include/ml/version.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace ml {
+
+constexpr int VERSION_MAJOR = 1;
+constexpr int VERSION_MINOR = 0;
+constexpr int VERSION_PATCH = 0;
+constexpr const char* VERSION = "1.0.0";
+
+} // namespace ml

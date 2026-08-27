0f5b24d8f7327ee9039839dcf603c772d7ad3c4c
bagdanchik007 <bogdanskibitskyi@gmail.com>
2026-08-27 15:38:13 +0200

    feat(demo): extend main demo

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -4,16 +4,29 @@ namespace ml::examples {
 
 void run_linear_regression_demo();
 void run_kmeans_demo();
+void run_preprocessing_demo();
+void run_feature_selection_demo();
 
 }
 
-int main() {
+int main()
+{
     std::cout << "=== Cpp ML Library Demo ===\n\n";
+
+    std::cout << "--- Algorithms ---\n\n";
 
     ml::examples::run_linear_regression_demo();
     ml::examples::run_kmeans_demo();
 
-    std::cout << "Demo finished successfully.\n";
+    std::cout << "\n--- Preprocessing ---\n\n";
+
+    ml::examples::run_preprocessing_demo();
+
+    std::cout << "\n--- Feature Selection ---\n\n";
+
+    ml::examples::run_feature_selection_demo();
+
+    std::cout << "\nDemo finished successfully.\n";
 
     return 0;
 }

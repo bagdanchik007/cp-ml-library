d182de6f6065c62b4e3b2ab7b480b73cda04f251
bagdanchik007 <bogdanskibitskyi@gmail.com>
2026-08-29 18:47:14 +0200

    fix(core): correct estimator interface

--- include/ml/core/estimators/estimator.hpp
+++ include/ml/core/estimators/estimator.hpp
@@ -1,18 +1,16 @@
-#pragma once 
+#pragma once
 
 #include "ml/core/matrix/matrix.hpp"
 
 namespace ml {
 
-    class Estiminator {
+class Estimator {
+public:
+    virtual ~Estimator() = default;
 
-        public:
-              virtual ~Estiminator() = default;
+    virtual void fit(const Matrix& X, const Matrix& y) = 0;
 
-              virtual void fit(const Matrix& X, const Matrix& y) = 0;
+    virtual Matrix predict(const Matrix& X) const = 0;
+};
 
-              virtual Matrix predict(const Matrix& X) const = 0;
-
-    };
-
-}// namespace ml
+} // namespace ml

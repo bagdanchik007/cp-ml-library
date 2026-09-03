77793a9233f79740bde61f29b20e027376a287eb
bagdanchik007 <bagdanchik007@users.noreply.github.com>
2026-09-03 23:16:45 +0200

    chore: update library version

--- include/ml/version.hpp
+++ include/ml/version.hpp
@@ -3,8 +3,8 @@
 namespace ml {
 
 constexpr int VERSION_MAJOR = 1;
-constexpr int VERSION_MINOR = 2;
+constexpr int VERSION_MINOR = 3;
 constexpr int VERSION_PATCH = 0;
-constexpr const char* VERSION = "1.2.0";
+constexpr const char* VERSION = "1.3.0";
 
 } // namespace ml
